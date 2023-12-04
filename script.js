f825352483ded2442a1c9170faca3f2f9384b8e0
Winklaa22 <49796412+Winklaa22@users.noreply.github.com>
2023-12-04 22:59:24 +0100

    visual effects

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-var searchButton = document.querySelector(".search-btn");
-var boxSelector = document.querySelector(".weather-box");
-var contentSelector = document.querySelector(".content")
-var cityText = document.getElementById("cityText");
+const searchButton = document.querySelector(".search-btn");
+const boxSelector = document.querySelector(".weather-box");
+const contentSelector = document.querySelector(".content")
+const cityText = document.getElementById("cityText");
+const errorPanel = document.querySelector(".errorPanel");
+const searchInput = document.querySelector(".city-input");
 let isOpened = false;
 
 let weatherInfoContent = null;
 
 searchButton.addEventListener('click', function() {
-    OpenFullBox();
+    onButtonClicked();
+   
+  });
+
+
+function onButtonClicked(){
+
+    if(searchInput.value == ""){
+        alert("wpisz coś kurwa");
+        return;
+    }
+        
+
     const API = 'c027370d3c86f74d371827244fbdffc3';
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${document.getElementById("searchInput").value}&units=metric&appid=${API}`)
     .then(response => response.json()).then(json => {
-
+        
+        if(json.cod == "404"){
+            OpenBox()
+            errorPanel.style.opacity = "1";
+            contentSelector.style.opacity = "0";
+            return;
+        }
+            
+        errorPanel.style.opacity = "0";
         const image = document.getElementById('weather-image');
         const desc = document.querySelector('.weather-desc')
         const temp = document.querySelector(".weather-degrees");
@@ -48,45 +70,27 @@ searchButton.addEventListener('click', function() {
             
         }
 
-        temp.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
-        desc.innerHTML = `${json.weather[0].description}`
+        temp.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        desc.innerHTML = `${json.weather[0].description}`;
 
-    });
-  });
-
-  function AddContent(){
-    weatherInfoContent = document.createElement('div');
-
-    weatherInfoContent.innerHTML = `
-    <div class="content">
-        <p class="city-text" id="cityText">Wrocław</p>
-        <img id="weather-image" src="weather-icons/cloudy.png" alt="">
-        <p class="weather-desc">Rainy</p>
-        <p class="weather-degrees">16<span>°C</span></p>
-    </div> 
-    `;
-
-    const container = document.querySelector('.container');
-    container.appendChild(weatherInfoContent)
-  }
-
-function OpenFullBox(){
-    const searchInput = document.querySelector(".city-input");
-
-    if(!isOpened){
-        AddContent();
-        searchInput.style.height = '30px';
-        boxSelector.style.height = '450px';
-        boxSelector.style.width = "400px"
-        contentSelector.style.opacity = '1';
-        cityText.innerHTML = document.getElementById("searchInput").value;
-    }
-    else{
-        contentSelector.style.opacity = '0';
-        setTimeout(()=>{
+        if(!isOpened){
+            OpenBox();
             contentSelector.style.opacity = '1';
             cityText.innerHTML = document.getElementById("searchInput").value;
-        }, 100)
-    }
-    isOpened =! isOpened;
+        }
+        else{
+            contentSelector.style.opacity = '0';
+            setTimeout(()=>{
+                contentSelector.style.opacity = '1';
+                cityText.innerHTML = document.getElementById("searchInput").value;
+            }, 100)
+        }
+        isOpened =! isOpened;
+    });
+}
+
+function OpenBox(){
+    searchInput.style.height = '30px';
+    boxSelector.style.height = '450px';
+    boxSelector.style.width = "400px"
 }
